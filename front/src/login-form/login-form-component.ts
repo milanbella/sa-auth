502ab86062916b0dff01327b5ecadc7307fc5a78
Milan Bella <milanbella>
2025-10-23 19:28:51 +0200

    m

--- front/src/login-form/login-form-component.ts
+++ front/src/login-form/login-form-component.ts
@@ -10,9 +10,41 @@ tpl.innerHTML = `
 
 class LoginForm extends HTMLElement {
     private shadow = this.attachShadow({mode: "open"});
+    private form: HTMLFormElement | null = null;
+    public onSubmit?: (username: string, password: string) => void;
+
+    private handleSubmit = (event: Event) => {
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement | null;
+        if (!form) {
+            return;
+        }
+
+        const formData = new FormData(form);
+        const username = (formData.get('username')) as string;
+        const password = (formData.get('password')) as string;
+
+        if (typeof this.onSubmit === 'function') {
+            this.onSubmit(username, password);
+        }
+    };
 
     connectedCallback() {
-        this.shadow.appendChild(tpl.content.cloneNode(true));
+        if (!this.shadow.hasChildNodes()) {
+            this.shadow.appendChild(tpl.content.cloneNode(true));
+        }
+
+        this.form = this.shadow.querySelector('form');
+        if (!this.form) {
+            console.error('no form');
+        }
+        this.form?.addEventListener('submit', this.handleSubmit);
+    }
+
+    disconnectedCallback() {
+        this.form?.removeEventListener('submit', this.handleSubmit);
+        this.form = null;
     }
 }
 
